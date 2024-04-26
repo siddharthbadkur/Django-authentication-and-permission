365c7db79da251ab8a795ab1a3450e65e2913184
siddharth <badkursiddharth282@gmail.com>
2024-04-26 14:22:36 +0530

    first commit in authentication and permission

--- project/api/urls.py
+++ project/api/urls.py
@@ -13,5 +13,5 @@ router.register(r'student', StudentViewSet)
 # specify URL Path for rest_framework
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')) # this line is used for only login & logout feature in DRF window
+     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')) # this line is used for only login & logout feature in DRF window
 ]
--- project/project/settings.py
+++ project/project/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        
+        # 'rest_framework.authentication.BasicAuthentication',
     ]
 }
 
